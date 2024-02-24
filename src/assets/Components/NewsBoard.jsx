23e151a6f0e875c105c8ede27daac663eb4adb6a
aitiqad <skr03ar@gmail.com>
2024-02-24 20:59:40 +0530

    fix: api issues resolved

--- src/assets/Components/NewsBoard.jsx
+++ src/assets/Components/NewsBoard.jsx
@@ -5,15 +5,16 @@ export const NewsBoard = ({category}) => {
     const[articles, setArticles]= useState([]);
 
     useEffect(()=>{
-        let url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
-        fetch(url).then(response=> response.json()).then(data=>setArticles(data.articles));
+        let url = `https://newsdata.io/api/1/news?apikey=${import.meta.env.VITE_API_KEY}&q=${category}&country=in`
+        // let url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
+        fetch(url).then(response=> response.json()).then(data=>setArticles(data.results));
     },[category])
   return (
     <div>
         <h1 class="font-sans font-bold text-center m-3 text-2xl text-gray-800">Latest News</h1>
         <div class='flex flex-wrap justify-evenly'>
         {articles.map((news,index)=>{
-            return <NewsItem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url}/> 
+            return <NewsItem key={index} title={news.title} description={news.description} src={news.image_url} url={news.link}/> 
         })}</div>
     </div>
   )
